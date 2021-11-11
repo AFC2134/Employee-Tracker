7ddcf2e6fed9e8adcaf4ace99a7ad12965b7b187
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-11-11 11:46:44 -0600

    sql tables complete

--- db/schema.sql
+++ db/schema.sql
@@ -7,4 +7,21 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL 
 );
 
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2),
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
 
