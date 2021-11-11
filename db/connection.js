cda74d3aa0c8b5354dc9a1482efc9c783c5228de
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-11-10 23:28:46 -0600

    file tree, schema, coonection.js, import express to server

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', 
+    password:'Coon3y21', 
+    database: 'employee_tracker_db'
+});
+connection.connect(function(err){
+    if(err) throw err
+})
+module.exports = connection
